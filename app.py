3bda1db12de16bff7ec0cc825cda1542f78f2fde
Raspberrypi <dushiun@gmail.com>
2021-02-24 16:27:36 +0800

    first update

---
+++ app.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+from flask import Flask ,request
+from flask import render_template
+from picamera import PiCamera
+from time import sleep
+
+import requests
+app = Flask(__name__)
+"""
+@app.route('/')
+def index():
+    return 'Hello ATCA!!'    #回傳字串，讓使用看到
+
+
+@app.route("/", methods=['GET'])
+def index2():
+    name = request.args.get(key='name')
+    print(str(name))
+    return "Hello"+str(name)
+    """ 
+@app.route("/", methods=['GET'])
+def photo():
+    photo_enable = request.args.get(key='photo')
+    print(str(photo_enable))
+    if(str(photo_enable)=="ON"):
+        camera = PiCamera()
+        sleep(5)
+        camera.start_preview()
+        camera.capture('./image.jpg')
+        camera.stop_preview()
+    return "successful"
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=2224)
